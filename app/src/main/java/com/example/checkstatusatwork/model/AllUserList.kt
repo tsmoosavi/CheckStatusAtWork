f4970e516da4f12c4abd35724643dbd4f03dab1d
ts moosavi <tt.musavi@gmail.com>
2022-05-26 12:15:32 +0430

    add model

---
+++ app/src/main/java/com/example/checkstatusatwork/model/AllUserList.kt
@@ -0,0 +1,3 @@
+package com.example.checkstatusatwork.model
+
+class AllUserList : ArrayList<UserItem>()
---
+++ app/src/main/java/com/example/checkstatusatwork/model/UserItem.kt
@@ -0,0 +1,11 @@
+package com.example.checkstatusatwork.model
+
+data class UserItem(
+    val avatar: String,
+    val id: String,
+    val name: String,
+    val password: Int,
+    val status: String,
+    val updatedAt: String,
+//    val userName: String
+)

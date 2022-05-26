7c9584c0ad051a1b487b93c4a203d03d0f28ee56
ts moosavi <tt.musavi@gmail.com>
2022-05-26 11:07:53 +0430

    start KoinApplication in manifest

--- app/src/main/java/com/example/checkstatusatwork/ui/MainActivity.kt
+++ app/src/main/java/com/example/checkstatusatwork/ui/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.checkstatusatwork.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.checkstatusatwork.R
+import com.example.checkstatusatwork.ui.register_page.RegistrationPageViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    val viewModel: RegistrationPageViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

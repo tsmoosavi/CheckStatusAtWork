de5e258d3e152ae5dfec5df17a6d68f3a42125d7
ts moosavi <tt.musavi@gmail.com>
2022-05-26 22:05:05 +0430

    add LoginFragment and LoginViewModel

---
+++ app/src/main/java/com/example/checkstatusatwork/ui/login_page/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.checkstatusatwork.ui.login_page
+
+import androidx.lifecycle.ViewModel
+import com.example.checkstatusatwork.data.User.UserRepository
+
+class LoginViewModel(val repository: UserRepository): ViewModel() {
+}
